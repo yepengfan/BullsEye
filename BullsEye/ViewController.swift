2b38a28721dc9267991665593a94eaf75381be05
Yepeng Fan <yepeng.fan@hotmail.com>
2015-11-23 21:20:14 +1100

    Add popup menu
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -21,5 +21,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World",
+            message: "This is my first app!",
+            preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
     }
 }
